648c9d21b90048a6cb4ed2e00f98ad395d55e8af
Fallen_Breath <nellaforax@outlook.com>
2024-12-05 01:40:49 +0800

    remove all unwanted subprojects for easier build
    what we want is only worldedit-core

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,14 +2,14 @@ rootProject.name = "worldedit"
 
 include("worldedit-libs")
 
-include("worldedit-bukkit:adapters:adapter-legacy")
-include("worldedit-bukkit:adapters:adapter-1.17.1")
+//include("worldedit-bukkit:adapters:adapter-legacy")
+//include("worldedit-bukkit:adapters:adapter-1.17.1")
 
-listOf("bukkit", "core", "sponge", "fabric", "forge", "cli").forEach {
+listOf("core").forEach {
     include("worldedit-libs:$it")
     include("worldedit-$it")
 }
-include("worldedit-mod")
+//include("worldedit-mod")
 include("worldedit-libs:core:ap")
 
-include("worldedit-core:doctools")
+//include("worldedit-core:doctools")
